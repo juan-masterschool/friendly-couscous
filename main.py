7dbe9a371701e0755e48362af024ab069edd5977
juan-masterschool <juan@faculty.masterschool.com>
2025-10-22 13:01:45 +0200

    first frontend commit

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ print("Starting the repo")
 print("This print was added locally")
 print("This print was added through GitHub")
 print("change happening locally for authentication")
+print("local change for the new frontend feature branch")
 
